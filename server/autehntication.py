f343f1acc355b7e6d6a8e6d3152bb4f8e0ae4cff
NotaCSstudent <mahmudbabu94@gmail.com>
2021-06-18 14:16:41 -0400

    Add files via upload
    authentication stuff

---
+++ server/autehntication.py
@@ -0,0 +1,40 @@
+import pymongo
+from pymongo import collection
+from pymongo import database
+from pymongo.mongo_client import MongoClient
+
+client = pymongo.MongoClient('mongodb+srv://<NAME>:<PASSWORD>@cluster0.bxtpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+data = client["FTP_DB"]
+userpass = data['userpass']
+
+g = userpass.find_one({'user':'embee'})
+t = userpass.count_documents({})
+print(g)
+print(t)
+
+
+def New_User(name :str,password : str) -> bool:
+    name = name.lower()
+    if(userpass.find_one({'user': name}) != None):
+        return False ##another user with that name
+    else:
+        size = userpass.count_documents({})## index will be at the end of database
+        userpass.insert_one({'_id':size+1,'user':name,'pass':password})
+        return True ##New user added
+
+
+def Find_User(name : str, password : str) -> bool:
+    name = name.lower() ##makes the user name lowercase just in case the user makes a mistake
+    Auth_Boi = userpass.find_one({'user':name})##Finds the user name first
+    if(Auth_Boi != None):
+        if(Auth_Boi['pass']==password):
+            return True## if the pass == password that will return true
+        else: 
+            return False
+    else:
+        return False
+
+
+t = Find_User('embee','1234')
+print(t)
